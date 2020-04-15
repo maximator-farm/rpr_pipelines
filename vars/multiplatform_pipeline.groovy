e7748954a058ffddca88f617a131f16086d8aaab
Andrey Kravchenko <epozine@inbox.ru>
2020-04-15 21:08:25 +0300

    Update multiplatform_pipeline.groovy

--- vars/multiplatform_pipeline.groovy
+++ vars/multiplatform_pipeline.groovy
@@ -161,7 +161,7 @@ def call(String platforms, def executePreBuild, def executeBuild, def executeTes
 
             // if timeout doesn't set - use default
             // value in minutes
-            options['PREBUILD_TIMEOUT'] = options['PREBUILD_TIMEOUT'] ?: 15
+            options['PREBUILD_TIMEOUT'] = options['PREBUILD_TIMEOUT'] ?: 60
             options['BUILD_TIMEOUT'] = options['BUILD_TIMEOUT'] ?: 60
             options['TEST_TIMEOUT'] = options['TEST_TIMEOUT'] ?: 60
             options['DEPLOY_TIMEOUT'] = options['DEPLOY_TIMEOUT'] ?: 60
