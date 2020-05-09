b90df4a644c7bd28a1927fab2eb86ce64df2bbda
GulDilin <zhenyagurin@gmail.com>
2020-05-09 21:55:55 +0300

    update run sh call params

--- vars/rpr_viewer_pipeline.groovy
+++ vars/rpr_viewer_pipeline.groovy
@@ -129,7 +129,7 @@ def executeTestCommand(String osName, String asicName, Map options)
                 sh """
                 chmod +x ../RprViewer/RadeonProViewer
                 chmod +x run.sh
-                ./run.sh ${options.testsPackage} \"${options.tests}\">> ../${options.stageName}.log  2>&1
+                ./run.sh ${options.testsPackage} \"${options.tests}\" ${build_id} ${job_id} ${universeClient.url} ${osName}-${asicName} ${universeClient.is_url} ${options.sendToRBS}>> ../${options.stageName}.log  2>&1
                 """
             }
         }
