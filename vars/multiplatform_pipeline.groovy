033438900af8dfff4b7ca057561ba4b0c025185e
Tarasenko Daniil <dtarasenko@luxoft.com>
2020-05-27 23:53:57 +0300

    remove debug print

--- vars/multiplatform_pipeline.groovy
+++ vars/multiplatform_pipeline.groovy
@@ -70,7 +70,6 @@ def executeTestsNode(String osName, String gpuNames, def executeTests, Map optio
                                             // add info about retry to options
                                             String tests = newOptions['tests']
                                             nodeRetryList.add("[\"nodeName\": \"${env.NODE_NAME}\", \"tests\": \"${tests}\", \"gpu\": \"${asicName}\"]")
-                                            println nodeRetryList.toString()
 
                                             // change PC after first failed tries and don't change in the last try
                                             if (i < nodesCount - 1 && nodesCount != 1) {
