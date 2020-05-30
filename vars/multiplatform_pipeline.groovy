f85684e63b7e7f1393351aca9f669bf94dd917cf
Tarasenko Daniil <dtarasenko@luxoft.com>
2020-05-30 12:29:31 +0300

    change json construction

--- vars/multiplatform_pipeline.groovy
+++ vars/multiplatform_pipeline.groovy
@@ -69,12 +69,17 @@ def executeTestsNode(String osName, String gpuNames, def executeTests, Map optio
 
                                             // add info about retry to options
                                             String tests = newOptions['tests']
-                                            // for (int i = 0; i < options.nodeReallocateTries; i++){
-                                            //     if nodeRetryList['Testers'].equals(nodesList){
-
-                                            //     }
-                                            // }
-                                            nodeRetryList.add("{\"Testers\":${nodesList.toString()}, \"Tries\": {\"${tests}\": [{\"host\": \"${env.NODE_NAME}\", \"link\": \"link_to_crash\"}]}}")
+                                            boolean added = false;
+                                            for (int i = 0; i < nodeRetryList.length(); i++){
+                                                if nodeRetryList[i]['Testers'].equals(nodesList){
+                                                    nodeRetryList[i]['Tries'][tests].add([host:env.NODE_NAME, link:'link_to_crash'])
+                                                    added = true
+                                                }
+                                            }
+                                            if (!added){
+                                                nodeRetryList.add([Testers: nodesList, Tries: [tests: [host:env.NODE_NAME, link:'link_to_crash']]])
+                                            }
+                                            //nodeRetryList.add("{\"Testers\":${nodesList.toString()}, \"Tries\": {\"${tests}\": [{\"host\": \"${env.NODE_NAME}\", \"link\": \"link_to_crash\"}]}}")
                                             println nodeRetryList.toString()
 
                                             // change PC after first failed tries and don't change in the last try
