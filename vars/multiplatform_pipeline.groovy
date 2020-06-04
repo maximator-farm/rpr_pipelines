650a551929c5108131ccc1085c060dfe0b267315
Tarasenko Daniil <dtarasenko@luxoft.com>
2020-06-04 15:32:29 +0300

    iterate nodeRetryList

--- vars/multiplatform_pipeline.groovy
+++ vars/multiplatform_pipeline.groovy
@@ -70,9 +70,9 @@ def executeTestsNode(String osName, String gpuNames, def executeTests, Map optio
                                             // add info about retry to options
                                             String tests = newOptions['tests']
                                             boolean added = false;
-                                            for (int try_num = 0; try_num < nodeRetryList.length; try_num++){
-                                                if (nodeRetryList[try_num]['Testers'].equals(nodesList)){
-                                                    nodeRetryList[try_num]['Tries'][tests].add([host:env.NODE_NAME, link:'link_to_crash'])
+                                            nodeRetryList.eachWithIndex{ it, i ->
+                                                if (it['Testers'].equals(nodesList)){
+                                                    it['Tries'][tests].add([host:env.NODE_NAME, link:'link_to_crash'])
                                                     added = true
                                                 }
                                             }
