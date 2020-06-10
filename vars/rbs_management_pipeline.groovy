cf4d7d10fe79a8131d331c36888b5428eb2486db
Dmitry Galkin <galkin.dmitry.485@gmail.com>
2020-06-10 07:13:46 +0300

    comment path

--- vars/rbs_management_pipeline.groovy
+++ vars/rbs_management_pipeline.groovy
@@ -31,14 +31,14 @@ def main(Map options) {
 		// 	println("[INFO] Old RBS compose stack doesn't exist")
 		// }
 
-		// dir('universe') {
-		checkOutBranchOrScm(options['universeBranch'], 'https://gitlab.cts.luxoft.com/dm1tryG/universe.git', false, false, true, 'radeonprorender-gitlab', false)
-		// }
+		dir('universe') {
+			checkOutBranchOrScm(options['universeBranch'], 'https://gitlab.cts.luxoft.com/dm1tryG/universe.git', false, false, true, 'radeonprorender-gitlab', false)
+		}
 
 		sshagent(credentials : ['FrontendMachineCredentials']) {
 			sh """
 				ssh ${options.user}@${options.frontendIp} mkdir -p ${options.RBSServicesRoot}/${version}
-				scp -r ./universe ${options.user}@${options.frontendIp}:${options.RBSServicesRoot}/${version}/universe
+				scp -r ./universe ${options.user}@${options.frontendIp}:${options.RBSServicesRoot}/${version}
 
 				ssh ${options.user}@${options.frontendIp} ${options.RBSServicesRoot}/${version}/universe/docker-management/build_pipeline.sh ${options.RBSServicesRoot}/${version}/universe/${dockerComposeFile}
 				ssh ${options.user}@${options.frontendIp} ${options.RBSServicesRoot}/${version}/universe/docker-management/up_pipeline.sh ${options.RBSServicesRoot}/${version}/universe/${dockerComposeFile}
