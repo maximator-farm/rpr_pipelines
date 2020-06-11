5ae2604ca7c388c2f88c6b704b55fe9fe37b5a90
Tarasenko Daniil <dtarasenko@luxoft.com>
2020-06-11 14:54:30 +0300

    fix .crash.log

--- vars/collectCrashInfo.groovy
+++ vars/collectCrashInfo.groovy
@@ -3,38 +3,38 @@ def call(osName, options){
     if (testsOrTestPackage == ''){
         testsOrTestPackage = options['testsPackage']
     }
-    logName = "${testsOrTestPackage}.${env.NODE_NAME}"
+    logName = "${testsOrTestPackage}.${env.NODE_NAME}.crash.log"
     println "Collect crash logs"
     switch(osName){
         case 'Windows':
             powershell """
-                echo ${env.NODE_NAME} >> ${logName}.crash.log
-                Get-Date >> ${logName}.crash.log
-                Get-EventLog -LogName System -Newest 200 >> ${logName}.crash.log
-                Get-EventLog -LogName Application -Newest 200 >> ${logName}.crash.log
-                Get-EventLog -LogName HardwareEvents -Newest 200 >> ${logName}.crash.log
-                ps | sort -des cpu | select -f 200 | ft -a >> ${logName}.crash.log
+                echo ${env.NODE_NAME} >> ${logName}
+                Get-Date >> ${logName}
+                Get-EventLog -LogName System -Newest 200 >> ${logName}
+                Get-EventLog -LogName Application -Newest 200 >> ${logName}
+                Get-EventLog -LogName HardwareEvents -Newest 200 >> ${logName}
+                ps | sort -des cpu | select -f 200 | ft -a >> ${logName}
             """
             break;
         case 'OSX':
             sh """
-                echo ${env.NODE_NAME} >> ${logName}.crash.log
-                date >> ${logName}.crash.log
-                log show --no-info --color always --predicate 'eventMessage CONTAINS[c] "radeon" OR eventMessage CONTAINS[c] "gpu" OR eventMessage CONTAINS[c] "amd"' --last 1h >> ${logName}.crash.log
-                top -l 1 | head -n 200 >> ${logName}.crash.log
-                sudo iotop | head -n 200 >> ${logName}.crash.log
+                echo ${env.NODE_NAME} >> ${logName}
+                date >> ${logName}
+                log show --no-info --color always --predicate 'eventMessage CONTAINS[c] "radeon" OR eventMessage CONTAINS[c] "gpu" OR eventMessage CONTAINS[c] "amd"' --last 1h >> ${logName}
+                top -l 1 | head -n 200 >> ${logName}
+                sudo iotop | head -n 200 >> ${logName}
             """
             break;
         default:
             sh """
-                echo ${env.NODE_NAME} >> ${logName}.crash.log
-                date >> ${logName}.crash.log
-                dmesg | tail -n 200 >> ${logName}.crash.log
-                top -b | head -n 200 >> ${logName}.crash.log
-                iotop --only -b | head -n 200 >> ${logName}.crash.log
+                echo ${env.NODE_NAME} >> ${logName}
+                date >> ${logName}
+                dmesg | tail -n 200 >> ${logName}
+                top -b | head -n 200 >> ${logName}
+                iotop --only -b | head -n 200 >> ${logName}
             """
             break;
     }
     archiveArtifacts artifacts: "*.crash.log"
-    stash includes: "${logName}.crash.log", name: "${logName}"
+    stash includes: "${logName}", name: "${logName}"
 }
