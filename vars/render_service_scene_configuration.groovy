5cf8a07e79a8c221b365ea57e554894ab8984620
Ilia Nemankov <INemankov@luxoft.com>
2020-07-08 13:35:55 +0200

    Change django url in scene configuration RS pipeline

--- vars/render_service_scene_configuration.groovy
+++ vars/render_service_scene_configuration.groovy
@@ -116,7 +116,7 @@ def main(Map options) {
 		options['PRJ_PATH']="${PRJ_PATH}"
 		options['JOB_PATH']="${JOB_PATH}"
 
-		options['django_url'] = "https://render.cis.luxoft.com/render/jenkins/"
+		options['django_url'] = "http://172.26.157.251:81/project/jenkins/"
 		options['scripts_branch'] = "inemankov/scene_scanning_blender"
 
 		String osName = 'Windows'
