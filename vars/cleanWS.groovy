74826af702ac8a2b37c893f7975a86a8576d86ba
Ilia Nemankov <49859594+Speedwag00n@users.noreply.github.com>
2020-08-25 21:07:48 +0200

    Abort cleanWS function if job was aborted (#250)

--- vars/cleanWS.groovy
+++ vars/cleanWS.groovy
@@ -1,8 +1,12 @@
+import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException;
+
 def call(String osName = "Windows") {
     try {
         println("[INFO] Try to clean WS via cleanWs command")
         cleanWs(deleteDirs: true, disableDeferredWipeout: true)
         println("[INFO] WS was successfully cleaned via cleanWs command")
+    } catch (FlowInterruptedException e1) {
+        throw e1
     } catch (Exception e1) {
 
         println("[ERROR] WS cleaning via cleanWs command failed. Try to kill processes and restart.")
@@ -15,7 +19,9 @@ def call(String osName = "Windows") {
                     taskkill /f /im "3dsmax.exe"
                 '''
             }
-        } catch (Exception e) {
+        } catch (FlowInterruptedException e2) {
+            throw e2
+        } catch (Exception e2) {
             // ignore errors if processes don't exist
         } 
 
@@ -23,11 +29,15 @@ def call(String osName = "Windows") {
             println("[INFO] Try to clean WS via cleanWs command again.")
             cleanWs(deleteDirs: true, disableDeferredWipeout: true)
             println("[INFO] WS was successfully cleaned via cleanWs command")
+        } catch (FlowInterruptedException e2) {
+            throw e2
         } catch (Exception e2) {
             try {
                 println("[ERROR] WS cleaning via cleanWs command failed. Try to do it via deleteDir command")
                 deleteDir()
                 println("[INFO] WS was successfully cleaned via deleteDir command")
+            } catch (FlowInterruptedException e3) {
+                throw e3
             } catch (Exception e3) {
                 println("[ERROR] WS cleaning via deleteDir command failed. Try to do it via OS commands")
                 switch(osName) {
