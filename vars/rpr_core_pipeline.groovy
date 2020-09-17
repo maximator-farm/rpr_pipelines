f7771f9129b18b1b03262433bc73e21f98cf43e3
Ilia Nemankov <INemankov@luxoft.com>
2020-09-17 08:27:15 +0200

    Fix usage oftotal count script in core pipeline

--- vars/rpr_core_pipeline.groovy
+++ vars/rpr_core_pipeline.groovy
@@ -573,7 +573,7 @@ def executeDeploy(Map options, List platformList, List testResultList)
             try {
                 dir("jobs_launcher") {
                     bat """
-                    count_lost_tests.bat \"${lostStashes}\" .. ..\\summaryTestResults default \"${options.tests}\"
+                    count_lost_tests.bat \"${lostStashes}\" .. ..\\summaryTestResults \"${options.splitTestsExecution}\" \"${options.testsPackage}\" \"${options.tests}\"
                     """
                 }
             } catch (e) {
