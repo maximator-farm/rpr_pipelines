a4fe24266cdf96c1942107295757578c7ee40d0a
Ilia Nemankov <INemankov@luxoft.com>
2020-10-08 18:56:54 +0200

    Fix rprviewer pipeline

--- vars/rpr_viewer_pipeline.groovy
+++ vars/rpr_viewer_pipeline.groovy
@@ -726,6 +726,7 @@ def executeDeploy(Map options, List platformList, List testResultList)
 
             try {
                 dir("jobs_launcher") {
+                    def skippedTests = JsonOutput.toJson(options.skippedTests)
                     bat """
                     count_lost_tests.bat \"${lostStashes}\" .. ..\\summaryTestResults \"${options.splitTestsExecution}\" \"${options.testsPackage}\" \"${options.tests.toString().replace(" ", "")}\" \"\" \"${escapeCharsByUnicode(skippedTests.toString())}\"
                     """
