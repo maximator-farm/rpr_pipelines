89ab87f738e80f9caf2643e4cbd7864a9313bb60
Ilia Nemankov <INemankov@luxoft.com>
2020-10-09 11:43:32 +0200

    Apply changes for other tools

--- vars/rpr_maxplugin_pipeline.groovy
+++ vars/rpr_maxplugin_pipeline.groovy
@@ -6,7 +6,8 @@ import net.sf.json.JSONSerializer
 import net.sf.json.JsonConfig
 import TestsExecutionType
 
-@Field UniverseClient universeClient = new UniverseClient(this, "https://umsapi.cis.luxoft.com", env, "https://imgs.cis.luxoft.com", "AMD%20Radeon™%20ProRender%20for%203ds%20Max")
+@Field UniverseClient universeClientProd = new UniverseClient(this, "https://umsapi.cis.luxoft.com", env, "https://imgs.cis.luxoft.com", "AMD%20Radeon™%20ProRender%20for%20Max")
+@Field UniverseClient universeClientDev = new UniverseClient(this, "http://172.26.157.233:5001", env, "https://imgs.cis.luxoft.com", "AMD%20Radeon™%20ProRender%20for%20Max")
 @Field ProblemMessageManager problemMessageManager = new ProblemMessageManager(this, currentBuild)
 
 
@@ -108,18 +109,26 @@ def executeTestCommand(String osName, String asicName, Map options)
 
     timeout(time: test_timeout, unit: 'MINUTES') { 
 
-        build_id = "none"
-        job_id = "none"
-        if (options.sendToUMS && universeClient.build != null){
-            build_id = universeClient.build["id"]
-            job_id = universeClient.build["job_id"]
+        String buildIdProd, buildIdDev, jobIdProd, jobIdDev, isUrl = "none"
+        if (options.sendToUMS && universeClientProd.build != null){
+            buildIdProd = universeClientProd.build["id"]
+            jobIdProd = universeClientProd.build["job_id"]
+            isUrl = universeClientProd.is_url
+        }
+        if (options.sendToUMS && universeClientDev.build != null){
+            buildIdDev = universeClientDev.build["id"]
+            jobIdDev = universeClientDev.build["job_id"]
+            isUrl = universeClientDev.is_url
         }
         withCredentials([usernamePassword(credentialsId: 'image_service', usernameVariable: 'IS_USER', passwordVariable: 'IS_PASSWORD'),
             usernamePassword(credentialsId: 'universeMonitoringSystem', usernameVariable: 'UMS_USER', passwordVariable: 'UMS_PASSWORD')])
         {
-            withEnv(["UMS_USE=${options.sendToUMS}", "UMS_BUILD_ID=${build_id}", "UMS_JOB_ID=${job_id}",
-                "UMS_URL=${universeClient.url}", "UMS_ENV_LABEL=${osName}-${asicName}", "IS_URL=${universeClient.is_url}",
-                "UMS_LOGIN=${UMS_USER}", "UMS_PASSWORD=${UMS_PASSWORD}", "IS_LOGIN=${IS_USER}", "IS_PASSWORD=${IS_PASSWORD}"])
+            withEnv(["UMS_USE=${options.sendToUMS}", "UMS_ENV_LABEL=${osName}-${asicName}",
+                "UMS_BUILD_ID_PROD=${buildIdProd}", "UMS_JOB_ID_PROD=${jobIdProd}", "UMS_URL_PROD=${universeClientProd.url}", 
+                "UMS_LOGIN_PROD=${UMS_USER}", "UMS_PASSWORD_PROD=${UMS_PASSWORD}",
+                "UMS_BUILD_ID_DEV=${buildIdDev}", "UMS_JOB_ID_DEV=${jobIdDev}", "UMS_URL_DEV=${universeClientDev.url}",
+                "UMS_LOGIN_DEV=${UMS_USER}", "UMS_PASSWORD_DEV=${UMS_PASSWORD}",
+                "IS_LOGIN=${IS_USER}", "IS_PASSWORD=${IS_PASSWORD}", "IS_URL=${isUrl}"])
             {
                 dir('scripts')
                 {
@@ -136,7 +145,8 @@ def executeTestCommand(String osName, String asicName, Map options)
 def executeTests(String osName, String asicName, Map options)
 {
     if (options.sendToUMS){
-        universeClient.stage("Tests-${osName}-${asicName}", "begin")
+        universeClientProd.stage("Tests-${osName}-${asicName}", "begin")
+        universeClientDev.stage("Tests-${osName}-${asicName}", "begin")
     }
     // used for mark stash results or not. It needed for not stashing failed tasks which will be retried.
     Boolean stashResults = true
@@ -254,7 +264,8 @@ def executeTests(String osName, String asicName, Map options)
 
                         if (options.sendToUMS)
                         {
-                            universeClient.stage("Tests-${osName}-${asicName}", "end")
+                            universeClientProd.stage("Tests-${osName}-${asicName}", "end")
+                            universeClientDev.stage("Tests-${osName}-${asicName}", "end")
                         }
 
                         if (sessionReport.summary.error > 0) {
@@ -349,7 +360,8 @@ def executeBuild(String osName, Map options)
 {
     cleanWS(osName)
     if (options.sendToUMS){
-        universeClient.stage("Build-" + osName , "begin")
+        universeClientProd.stage("Build-" + osName , "begin")
+        universeClientDev.stage("Build-" + osName , "begin")
     }
     try {
         dir('RadeonProRenderMaxPlugin')
@@ -398,7 +410,8 @@ def executeBuild(String osName, Map options)
         archiveArtifacts artifacts: "*.log", allowEmptyArchive: true
     }
     if (options.sendToUMS){
-        universeClient.stage("Build-" + osName, "end")
+        universeClientProd.stage("Build-" + osName, "end")
+        universeClientDev.stage("Build-" + osName, "end")
     }
 }
 
@@ -655,10 +668,12 @@ def executePreBuild(Map options)
         {
             // Universe : auth because now we in node
             // If use httpRequest in master slave will catch 408 error
-            universeClient.tokenSetup()
+            universeClientProd.tokenSetup()
+            universeClientDev.tokenSetup()
 
             // create build ([OS-1:GPU-1, ... OS-N:GPU-N], ['Suite1', 'Suite2', ..., 'SuiteN'])
-            universeClient.createBuild(options.universePlatforms, options.groupsUMS)
+            universeClientProd.createBuild(options.universePlatforms, options.groupsUMS)
+            universeClientDev.createBuild(options.universePlatforms, options.groupsUMS)
         }
         catch (e)
         {
@@ -847,7 +862,8 @@ def executeDeploy(Map options, List platformList, List testResultList)
             if (options.sendToUMS) {
                 try {
                     String status = currentBuild.result ?: 'SUCCESSFUL'
-                    universeClient.changeStatus(status)
+                    universeClientProd.changeStatus(status)
+                    universeClientDev.changeStatus(status)
                 } catch (e) {
                     println(e.getMessage())
                 }
@@ -1024,7 +1040,8 @@ def call(String projectRepo = "git@github.com:GPUOpen-LibrariesAndSDKs/RadeonPro
     {
         currentBuild.result = "FAILURE"
         if (sendToUMS){
-            universeClient.changeStatus(currentBuild.result)
+            universeClientProd.changeStatus(currentBuild.result)
+            universeClientDev.changeStatus(currentBuild.result)
         }
         println(e.toString());
         println(e.getMessage());
