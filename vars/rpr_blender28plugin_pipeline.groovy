7b2c8a9151371cdafbc4da5e125ce1245d13fe69
Andrei Kravchenko <epozine@inbox.ru>
2020-10-15 12:09:59 +0300

    Turn back timeouts for maya & blender

--- vars/rpr_blender28plugin_pipeline.groovy
+++ vars/rpr_blender28plugin_pipeline.groovy
@@ -1416,7 +1416,7 @@ def call(String projectRepo = "git@github.com:GPUOpen-LibrariesAndSDKs/RadeonPro
                         splitTestsExecution:splitTestsExecution,
                         sendToUMS: sendToUMS,
                         gpusCount:gpusCount,
-                        TEST_TIMEOUT:60,
+                        TEST_TIMEOUT:180,
                         ADDITIONAL_XML_TIMEOUT:30,
                         NON_SPLITTED_PACKAGE_TIMEOUT:120,
                         DEPLOY_TIMEOUT:deployTimeout,
