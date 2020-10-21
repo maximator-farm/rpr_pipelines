010700888a7e04998e4e12a2a7318aedb0bc7695
Andrei Kravchenko <epozine@inbox.ru>
2020-10-21 22:28:50 +0300

    Fix commit message display in job

--- vars/radeon_ml_pipeline.groovy
+++ vars/radeon_ml_pipeline.groovy
@@ -230,7 +230,7 @@ def executePreBuild(Map options)
     checkOutBranchOrScm(options['projectBranch'], options['projectRepo'])
 
     options.commitAuthor = bat (script: "git show -s --format=%%an HEAD ",returnStdout: true).split('\r\n')[2].trim()
-    options.commitMessage = bat (script: "git log --format=%%B -n 1", returnStdout: true)
+    options.commitMessage = bat (script: "git log --format=%%B -n 1", returnStdout: true).split('\r\n')[2].trim()
     options.commitSHA = bat (script: "git log --format=%%H -1 ", returnStdout: true).split('\r\n')[2].trim()
     println "The last commit was written by ${options.commitAuthor}."
     println "Commit message: ${options.commitMessage}"
