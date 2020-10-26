9f9ca5c88368ddb7b220346233da8d04a2b7ac0b
Ilia Nemankov <INemankov@luxoft.com>
2020-10-26 20:20:09 +0300

    Fix ignoring of cache dir

--- vars/rpr_core_pipeline.groovy
+++ vars/rpr_core_pipeline.groovy
@@ -302,7 +302,7 @@ def executeTests(String osName, String asicName, Map options)
                         }
 
                         echo "Stashing test results to : ${options.testResultsName}"
-                        stash includes: '**/*', excludes: '**/cache/*', name: "${options.testResultsName}", allowEmpty: true
+                        stash includes: '**/*', excludes: '**/cache/**', name: "${options.testResultsName}", allowEmpty: true
 
                         // reallocate node if there are still attempts
                         if (sessionReport.summary.total == sessionReport.summary.error + sessionReport.summary.skipped || sessionReport.summary.total == 0) {
