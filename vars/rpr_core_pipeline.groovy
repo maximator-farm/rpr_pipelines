90bdb1f29c2f2310c21cf0a30c38c1d5c8f3f8a8
Ilia Nemankov <49859594+Speedwag00n@users.noreply.github.com>
2020-11-02 21:54:09 +0300

    Fix core total count (#356)

--- vars/rpr_core_pipeline.groovy
+++ vars/rpr_core_pipeline.groovy
@@ -670,7 +670,7 @@ def executeDeploy(Map options, List platformList, List testResultList)
             try {
                 dir("jobs_launcher") {
                     bat """
-                    count_lost_tests.bat \"${lostStashes}\" .. ..\\summaryTestResults \"${options.splitTestsExecution}\" \"${options.testsPackage}\" \"${options.tests.toString().replace(" ", "")}\" \"\" \"{}\"
+                    count_lost_tests.bat \"${lostStashes}\" .. ..\\summaryTestResults \"${options.splitTestsExecution}\" \"${options.testsPackage}\" \"${options.tests.toString()}\" \"\" \"{}\"
                     """
                 }
             } catch (e) {
