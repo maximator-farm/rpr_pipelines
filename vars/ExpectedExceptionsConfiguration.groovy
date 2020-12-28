07c993098613d3e9ff1c1275747cbcd304fcb2a9
Ilia Nemankov <INemankov@luxoft.com>
2020-12-28 20:24:02 +0300

    Fix syntax in ExpectedExecutionsConfiguration

--- vars/ExpectedExceptionsConfiguration.groovy
+++ vars/ExpectedExceptionsConfiguration.groovy
@@ -1,9 +1,5 @@
-public class ExpectedExecutionsConfiguration {
-    
-    BUILD_PLUGIN = [
-         ["class": Exception, "problemMessage": "Failed to build the plugin.", 
-         "rethrow": ExpectedExecutionThrowType.RETHROW, "scope": ProblemMessageManager.SPECIFIC,
-         "githubNotification": ["status": "failure", "message": "Failed to build the plugin."]]
-    ]
-
-}
+def static BUILD_PLUGIN = [
+     ["class": Exception, "problemMessage": "Failed to build the plugin.", 
+     "rethrow": ExpectedExecutionThrowType.RETHROW, "scope": ProblemMessageManager.SPECIFIC,
+     "githubNotification": ["status": "failure", "message": "Failed to build the plugin."]]
+]
