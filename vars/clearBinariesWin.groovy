a216d84cf35515e2ce3dd878b8cf5a00fc5dbcd3
Ilia Nemankov <49859594+Speedwag00n@users.noreply.github.com>
2021-02-14 11:47:34 +0300

    Fix clear binaries win function (#475)

--- vars/clearBinariesWin.groovy
+++ vars/clearBinariesWin.groovy
@@ -1,11 +1,18 @@
 def call() {
-    powershell"""
-        if (Test-Path "${CIS_TOOLS}\\..\\PluginsBinaries") {
-            forfiles /p "${CIS_TOOLS}\\..\\PluginsBinaries" /s /d -2 /c "cmd /c del @file"
-            \$folderSize = (Get-ChildItem -Recurse \"${CIS_TOOLS}\\..\\PluginsBinaries\" | Measure-Object -Property Length -Sum).Sum / 1GB
-            if (\$folderSize -ge 10) {
-                Remove-Item -Recurse -Force \"${CIS_TOOLS}\\..\\PluginsBinaries\"
+    try {
+        powershell"""
+            if (Test-Path "${CIS_TOOLS}\\..\\PluginsBinaries") {
+                Get-ChildItem "${CIS_TOOLS}\\..\\PluginsBinaries" -Recurse -File | Where CreationTime -lt  (Get-Date).AddDays(-2)  | Remove-Item -Force
+                \$folderSize = (Get-ChildItem -Recurse \"${CIS_TOOLS}\\..\\PluginsBinaries\" | Measure-Object -Property Length -Sum).Sum / 1GB
+                if (\$folderSize -ge 10) {
+                    Remove-Item -Recurse -Force \"${CIS_TOOLS}\\..\\PluginsBinaries\"
+                }
             }
-        }
-    """
+        """
+    } catch (e) {
+        println("[ERROR] Can't clear Win binaries")
+        println(e.toString())
+        println(e.getMessage())
+    }
+
 }
