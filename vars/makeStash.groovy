36b1373e17e266f07b15c500657110f3c00aa296
Ilia Nemankov <INemankov@luxoft.com>
2021-03-01 20:55:03 +0300

    Add zip and unzip utilities usage during managing of stashed through NAS

--- vars/makeStash.groovy
+++ vars/makeStash.groovy
@@ -4,56 +4,66 @@ def call(Map params) {
 
     try {
         stashName = params["name"]
+
+        println("[INFO] Make stash with name '${stashName}'")
+
         Boolean allowEmpty = params["allowEmpty"]
         String includes = params["includes"]
         String excludes = params["excludes"]
         Boolean debug = params["debug"]
         String debugPostfix = debug ? "Debug" : ""
 
-        def sendParams = []
-
-        // the greates importance has include/exclude which is the first!
-
-        if (excludes) {
-            for (exclude in excludes.split(",")) {
-                sendParams << "--exclude '${exclude}'"
-            }
-        }
+        def zipParams = []
 
         if (includes) {
             for (include in includes.split(",")) {
-                sendParams << "--include '${include}'"
+                zipParams << "-i '${include}'"
             }
-        } else {
-            sendParams << "--include '**'"
         }
 
-        sendParams << "--include='*/' --exclude='*'"
+        if (excludes) {
+            for (exclude in excludes.split(",")) {
+                zipParams << "-x '${exclude}'"
+            }
+        }
 
-        sendParams = sendParams.join(" ")
+        zipParams = zipParams.join(" ")
 
         String remotePath = "/volume1/Stashes/${env.JOB_NAME}/${env.BUILD_NUMBER}/${stashName}/"
 
         String stdout
 
-        withCredentials([string(credentialsId: "nasURL", variable: "REMOTE_HOST")]) {
+        String zipName = "stash_${stashName}.zip"
+
+        if (!zipName.endsWith(".zip")) {
             if (isUnix()) {
-                stdout = sh(returnStdout: true, 
-                    script: "${CIS_TOOLS}/stash${debugPostfix}.sh $REMOTE_HOST ${remotePath} \"${sendParams}\"")
+                stdout = sh(returnStdout: true, script: "zip -r ${zipName} . ${zipParams}")
             } else {
-                stdout = bat(returnStdout: true, 
-                    script: "%CIS_TOOLS%\\stash${debugPostfix}.bat %REMOTE_HOST% ${remotePath} \"${sendParams}\"")
+                stdout = bat(returnStdout: true, script: "bash -c \"zip -r ${zipName} . ${zipParams}\"")
             }
+        } else {
+            utils.renameFile(this, isUnix() ? "Windows" : "Unix", zipName, "stash_${stashName}.ziped")
         }
 
-        if (debugPostfix) {
+        zipName = "stash_${stashName}.ziped"
+
+        if (debug) {
             println(stdout)
         }
 
-        if (stdout.contains("total size is 0 ") && !allowEmpty) {
+        if (stdout.contains("Nothing to do!") && !allowEmpty) {
             println("[ERROR] Stash is empty")
             throw new Exception("Empty stash")
         }
+
+        withCredentials([string(credentialsId: "nasURL", variable: "REMOTE_HOST")]) {
+            if (isUnix()) {
+                sh(script: '$CIS_TOOLS/stash.sh' + " ${zipName} ${remotePath} " + '$REMOTE_HOST')
+            } else {
+                bat(script: '%CIS_TOOLS%\\stash.bat' + " ${zipName} ${remotePath} " + '%REMOTE_HOST%')
+            }
+        }
+
     } catch (e) {
         println("[ERROR] Failed to make stash with name '${stashName}'")
         println(e.toString())
--- vars/makeUnstash.groovy
+++ vars/makeUnstash.groovy
@@ -1,5 +1,7 @@
 def call(String stashName, Boolean debug = false) {
 
+    println("[INFO] Make unstash of stash with name '${stashName}'")
+
     String debugPostfix = debug ? "Debug" : ""
 
     String remotePath = "/volume1/Stashes/${env.JOB_NAME}/${env.BUILD_NUMBER}/${stashName}/"
@@ -9,15 +11,29 @@ def call(String stashName, Boolean debug = false) {
     try {
         withCredentials([string(credentialsId: "nasURL", variable: "REMOTE_HOST")]) {
             if (isUnix()) {
-                stdout = sh(returnStdout: true, script: "${CIS_TOOLS}/unstash${debugPostfix}.sh $REMOTE_HOST ${remotePath}")
+                sh(script: '$CIS_TOOLS/unstash.sh' + " ${remotePath} " + '$REMOTE_HOST')
             } else {
-                stdout = bat(returnStdout: true, script: "%CIS_TOOLS%\\unstash${debugPostfix}.bat %REMOTE_HOST% ${remotePath}")
+                bat(script: '%CIS_TOOLS%\\unstash.bat' + " ${remotePath} " + '$REMOTE_HOST')
             }
+        }
+
+        if (fileExists("stash_${stashName}.ziped")) {
+            utils.renameFile(this, isUnix() ? "Windows" : "Unix", "stash_${stashName}.ziped", "stash_${stashName}.zip")
+        } else if (fileExists("stash_${stashName}.zip")) {
+            if (isUnix()) {
+                stdout = sh(returnStdout: true, script: "unzip -u stash_${stashName}.zip")
+            } else {
+                stdout = bat(returnStdout: true, script: "bash -c \"unzip -u stash_${stashName}.zip\"")
+            }
+        } else {
+            println("[ERROR] Could not find any suitable archive with stash content")
+            throw new Exception("Could not find any suitable archive with stash content")
         }
 
         if (debugPostfix) {
             println(stdout)
         }
+
     } catch (e) {
         println("Failed to unstash stash with name '${stashName}'")
         println(e.toString())
