c01e85b3b7e1262dee034bbed361f0ec967f1c58
Ilia Nemankov <49859594+Speedwag00n@users.noreply.github.com>
2021-03-30 19:23:31 +0300

    Refer to release log in ML pipeline (#552)
    * Fix package in USD Viewer auto
    
    * Refer to release log in ML pipeline

--- vars/radeon_ml_pipeline.groovy
+++ vars/radeon_ml_pipeline.groovy
@@ -441,7 +441,7 @@ def executeBuild(String osName, Map options)
     } finally {
         if (env.CHANGE_ID) {
             String status = error_message ? "failure" : "success"
-            pullRequest.createStatus("${status}", context, "Build finished as '${status}'", "${env.BUILD_URL}/artifact/${STAGE_NAME}.log")
+            pullRequest.createStatus("${status}", context, "Build finished as '${status}'", "${env.BUILD_URL}/artifact/${STAGE_NAME}_Release.log")
             options['commitContexts'].remove(context)
         }
         archiveArtifacts "*.log"
