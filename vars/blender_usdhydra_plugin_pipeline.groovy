b8b2f2e3efd873045bbca6b09853d6b0d0130905
Ilia Nemankov <49859594+Speedwag00n@users.noreply.github.com>
2021-03-30 12:01:02 +0300

    Switch USD Blender Hydra to Blender 2.93 (#548)

--- vars/blender_usdhydra_plugin_pipeline.groovy
+++ vars/blender_usdhydra_plugin_pipeline.groovy
@@ -891,7 +891,7 @@ def appendPlatform(String filteredPlatforms, String platform) {
 def call(String projectRepo = "git@github.com:GPUOpen-LibrariesAndSDKs/BlenderUSDHydraAddon.git",
     String projectBranch = "",
     String testsBranch = "master",
-    String platforms = 'Windows;Ubuntu18',
+    String platforms = 'Windows:AMD_RXVEGA,AMD_WX9100,AMD_WX7100,NVIDIA_GF1080TI,AMD_RadeonVII,AMD_RX5700XT,AMD_RX6800;Ubuntu18',
     String updateRefs = 'No',
     Boolean enableNotifications = true,
     Boolean incrementVersion = true,
@@ -906,7 +906,7 @@ def call(String projectRepo = "git@github.com:GPUOpen-LibrariesAndSDKs/BlenderUS
     String customBuildLinkOSX = "",
     String enginesNames = "RRP,GL",
     String tester_tag = "Blender2.8",
-    String toolVersion = "2.91",
+    String toolVersion = "2.93",
     String mergeablePR = "",
     String parallelExecutionTypeString = "TakeAllNodes",
     Integer testCaseRetries = 3
