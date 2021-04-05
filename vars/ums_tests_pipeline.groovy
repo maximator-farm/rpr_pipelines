c367b673ad9f95d1d5742f1d8ce08890ebaf0641
GulDilin <zhenyagurin@gmail.com>
2021-04-06 02:17:21 +0300

    add ums_tests_pipeline

---
+++ vars/ums_tests_pipeline.groovy
@@ -0,0 +1,85 @@
+import groovy.transform.Field
+import universe.*
+
+
+@Field final String PRODUCT_NAME = "UMS+Tests"
+
+
+/**
+* Tests execution methods
+*
+* @param testsBranch jobs_launcher branch
+* @param envs List of envs labels
+* @param test_groups List of test groups names
+*/
+def execute(
+    String testsBranch,
+    List envs,
+    List test_groups
+) {
+    def client = null
+
+    universeClientProd = new UniverseClient(this, universeURLProd, env, imageServiceURL, PRODUCT_NAME);
+
+    withCredentials([string(credentialsId: "devTestUmsURL", variable: "TEST_UMS_URL"),
+            string(credentialsId: "imageServiceURL", variable: "IS_URL")]) {
+
+        umsURL = TEST_UMS_URL
+        imageServiceURL = IS_URL
+        println("[INFO] START Create UMS client")
+        client = new UniverseClient(this, umsURL, env, imageServiceURL, productName)
+        println("[INFO] END Create UMS client")
+    }
+
+    println("[INFO] START Setup token")
+    client.tokenSetup()
+    println("[INFO] END Setup Token")
+
+    println("[INFO] START Create build")
+    withCredentials([string(credentialsId: "devTestFrontUmsURL", variable: "TEST_UMS_FRONT_URL")]) {
+        universeClientProd.createBuild(envs, test_groups, false, null, TEST_UMS_FRONT_URL, "test")
+    }
+    println("[INFO] END Create build")
+
+    BUILD_ID = client.build["id"]
+    println("[INFO] BUILD_ID = ${BUILD_ID}")
+    JOB_ID = client.build["job_id"]
+    println("[INFO] JOB_ID = ${JOB_ID}")
+
+    cleanWS("Ubuntu")
+    checkoutScm(branchName: testsBranch, repositoryUrl: "git@github.com:luxteam/jobs_launcher.git")
+
+    client.stage("Tests", "begin");
+
+    println("[INFO] START Tests execution")
+    envs.each { env_label ->
+        println("[INFO] START Environment tests execution")
+        withCredentials([usernamePassword(credentialsId: 'universeMonitoringSystem', usernameVariable: 'UMS_USER', passwordVariable: 'UMS_PASSWORD')]) {
+            sh """
+                sudo sh run_ums_tests.sh ${umsURL} ${UMS_USER} ${UMS_PASSWORD} ${JOB_ID} ${BUILD_ID} ${env_label} ${test_groups.join(',')}>> ../tests.log 2>&1
+            """
+        }
+    }
+    println("[INFO] END Tests execution")
+
+    client.stage("Tests", "end");
+}
+
+/**
+* Pipeline for run UMS tests
+*
+* @param testsBranch jobs_launcher branch
+*/
+def call(
+    String testsBranch,
+) {
+
+    node("UMS") {
+        execute(
+            testsBranch,
+            ["Windows", "Ubuntu"],
+            ["Smoke", "Example"]
+        )
+
+    }
+}
