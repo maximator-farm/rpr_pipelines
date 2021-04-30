0c60f826aba5323a575d0f5cea2e1381c1d19868
Andrei Kravchenko <epozine@inbox.ru>
2021-04-30 13:10:51 +0300

    Update houdini scripts

--- vars/rpr_usdplugin_pipeline.groovy
+++ vars/rpr_usdplugin_pipeline.groovy
@@ -444,7 +444,7 @@ def executeBuildUnix(String osName, Map options) {
         if (env.HOUDINI_INSTALLATION_PATH) {
             installation_path = "${env.HOUDINI_INSTALLATION_PATH}"
         } else {
-            installation_path = "/home/admin"
+            installation_path = "/home/${env.USERNAME}"
         }
         if (options.buildType == "Houdini") {
             options.unix_houdini_python3 = options.houdini_python3 ? "-py3" : "-py2"
