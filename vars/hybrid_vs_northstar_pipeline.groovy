91f920499a09583603bed6b6f2a7767b018e2d11
Ilia Nemankov <INemankov@luxoft.com>
2021-05-05 18:21:54 +0300

    Init commit for HybridVsNorthstar pipeline

---
+++ vars/hybrid_vs_northstar_pipeline.groovy
@@ -0,0 +1,200 @@
+def executeBuildWindows(Map options) {
+    dir('HybridVsNorthStar') {
+        GithubNotificator.updateStatus("Build", "Windows", "in_progress", options, NotificationConfiguration.BUILD_SOURCE_CODE_START_MESSAGE, "${BUILD_URL}/artifact/Build-Windows.log")
+        
+        bat """
+            cmake ./ -B ./build >> Build-Windows.log 2>&1
+        """
+
+        dir("build") {
+            bat """
+                cmake --build ./ --config Release >> ../Build-Windows.log 2>&1
+            """
+
+            dir("build\\release") {
+                zip archive: true, zipFile: "HybridVsNorthStar_Windows.zip"
+            }
+
+            GithubNotificator.updateStatus("Build", "Windows", "success", options, NotificationConfiguration.BUILD_SOURCE_CODE_END_MESSAGE, pluginUrl)
+        }
+    }
+}
+
+
+def executeBuild(String osName, Map options) {
+    try {
+        dir("HybridVsNorthStar") {
+            withNotifications(title: osName, options: options, configuration: NotificationConfiguration.DOWNLOAD_SOURCE_CODE_REPO) {
+                checkoutScm(branchName: options.projectBranch, repositoryUrl: options.projectRepo, prBranchName: options.prBranchName, prRepoName: options.prRepoName)
+            }
+        }
+
+        outputEnvironmentInfo(osName)
+
+        withNotifications(title: osName, options: options, configuration: NotificationConfiguration.BUILD_SOURCE_CODE) {
+            switch(osName) {
+                case "Windows":
+                    executeBuildWindows(options)
+                    break
+                case "OSX":
+                    println("Unsupported OS")
+                    break
+                default:
+                    println("Unsupported OS")
+            }
+        }
+    } catch (e) {
+        throw e
+    } finally {
+        archiveArtifacts artifacts: "*.log", allowEmptyArchive: true
+    }
+}
+
+
+def executePreBuild(Map options)
+{
+    // auto job
+    if (env.BRANCH_NAME) {
+        if (env.CHANGE_URL) {
+            println "[INFO] Branch was detected as Pull Request"
+            options.executeBuild = true
+            options.executeTests = true
+            options.testsPackage = "regression.json"
+        } else if (env.BRANCH_NAME == "master" || env.BRANCH_NAME == "develop") {
+           println "[INFO] ${env.BRANCH_NAME} branch was detected"
+           options['executeBuild'] = true
+           options['executeTests'] = true
+           options['testsPackage'] = "regression.json"
+        } else {
+            println "[INFO] ${env.BRANCH_NAME} branch was detected"
+            options['testsPackage'] = "regression.json"
+        }
+    // manual job
+    } else {
+        println "[INFO] Manual job launch detected"
+        options['executeBuild'] = true
+        options['executeTests'] = true
+    }
+
+    dir('HybridVsNorthStar') {
+        withNotifications(title: "Jenkins build configuration", options: options, configuration: NotificationConfiguration.DOWNLOAD_SOURCE_CODE_REPO) {
+            checkoutScm(branchName: options.projectBranch, repositoryUrl: options.projectRepo, disableSubmodules: true)
+        }
+
+        options.commitAuthor = bat (script: "git show -s --format=%%an HEAD ",returnStdout: true).split('\r\n')[2].trim()
+        options.commitMessage = bat (script: "git log --format=%%B -n 1", returnStdout: true).split('\r\n')[2].trim()
+        options.commitSHA = bat (script: "git log --format=%%H -1 ", returnStdout: true).split('\r\n')[2].trim()
+        options.commitShortSHA = options.commitSHA[0..6]
+
+        println(bat (script: "git log --format=%%s -n 1", returnStdout: true).split('\r\n')[2].trim())
+        println "The last commit was written by ${options.commitAuthor}."
+        println "Commit message: ${options.commitMessage}"
+        println "Commit SHA: ${options.commitSHA}"
+        println "Commit shortSHA: ${options.commitShortSHA}"
+
+        if (options.projectBranch) {
+            currentBuild.description = "<b>Project branch:</b> ${options.projectBranch}<br/>"
+        } else {
+            currentBuild.description = "<b>Project branch:</b> ${env.BRANCH_NAME}<br/>"
+        }
+
+        withNotifications(title: "Jenkins build configuration", options: options, configuration: NotificationConfiguration.INCREMENT_VERSION) {
+            if (options['incrementVersion']) {
+                withNotifications(title: "Jenkins build configuration", printMessage: true, options: options, configuration: NotificationConfiguration.CREATE_GITHUB_NOTIFICATOR) {
+                    GithubNotificator githubNotificator = new GithubNotificator(this, options)
+                    githubNotificator.init(options)
+                    options["githubNotificator"] = githubNotificator
+                    githubNotificator.initPreBuild("${BUILD_URL}")
+                }
+            }
+
+            currentBuild.description += "<b>Version:</b> ${options.pluginVersion}<br/>"
+            currentBuild.description += "<b>Commit author:</b> ${options.commitAuthor}<br/>"
+            currentBuild.description += "<b>Commit message:</b> ${options.commitMessage}<br/>"
+            currentBuild.description += "<b>Commit SHA:</b> ${options.commitSHA}<br/>"
+        }
+    }
+
+    if (env.BRANCH_NAME && options.githubNotificator) {
+        options.githubNotificator.initChecks(options, "${BUILD_URL}")
+    }
+
+    def tests = []
+    options.timeouts = [:]
+
+    withNotifications(title: "Jenkins build configuration", options: options, configuration: NotificationConfiguration.CONFIGURE_TESTS) {
+        // TODO configure tests
+    }
+
+    stash includes: "resources/", name: "testResources", allowEmpty: false
+    stash includes: "third_party/*.dll", name: "enginesDlls", allowEmpty: false
+}
+
+
+def call(String projectRepo = "git@github.com:Radeon-Pro/HybridVsNorthStar.git",
+    String projectBranch = "",
+    String testsBranch = "master",
+    String platforms = 'Windows',
+    String updateRefs = 'No',
+    Boolean enableNotifications = true,
+    String testsPackage = "",
+    String tests = "",
+    Boolean splitTestsExecution = true,
+    String parallelExecutionTypeString = "TakeAllNodes"
+    )
+{
+    ProblemMessageManager problemMessageManager = new ProblemMessageManager(this, currentBuild)
+    Map options = [:]
+    options["stage"] = "Init"
+    options["problemMessageManager"] = problemMessageManager
+
+    def nodeRetry = []
+
+    try {
+        withNotifications(options: options, configuration: NotificationConfiguration.INITIALIZATION) {
+            gpusCount = 0
+            platforms.split(';').each() { platform ->
+                List tokens = platform.tokenize(':')
+                if (tokens.size() > 1) {
+                    gpuNames = tokens.get(1)
+                    gpuNames.split(',').each() {
+                        gpusCount += 1
+                    }
+                }
+            }
+
+            def parallelExecutionType = TestsExecutionType.valueOf(parallelExecutionTypeString)
+
+            println "Platforms: ${platforms}"
+            println "Tests: ${tests}"
+            println "Tests package: ${testsPackage}"
+            println "Split tests execution: ${splitTestsExecution}"
+            println "Tests execution type: ${parallelExecutionType}"
+
+            options << [projectRepo:projectRepo,
+                        projectBranch:projectBranch,
+                        testsBranch:testsBranch,
+                        updateRefs:updateRefs,
+                        enableNotifications:enableNotifications,
+                        testsPackage:testsPackage,
+                        tests:tests,
+                        splitTestsExecution:splitTestsExecution,
+                        gpusCount:gpusCount,
+                        nodeRetry: nodeRetry,
+                        platforms:platforms,
+                        parallelExecutionType:parallelExecutionType,
+                        parallelExecutionTypeString: parallelExecutionTypeString
+                        ]
+        }
+
+        multiplatform_pipeline(platforms, this.&executePreBuild, this.&executeBuild, null, null, options)
+    } catch(e) {
+        currentBuild.result = "FAILURE"
+        println(e.toString())
+        println(e.getMessage())
+        throw e
+    } finally {
+        problemMessageManager.publishMessages()
+    }
+
+}
