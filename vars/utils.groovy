193c55183460f62603b9236c1c4c8a28fd55c098
Flame0n <36414392+Flame0n@users.noreply.github.com>
2021-06-11 15:08:02 +0300

    Added loggining functions to Utils

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -7,6 +7,60 @@ import groovy.json.JsonOutput
  */
 class utils {
 
+    enum Color{
+        //text colors
+        RED("\033[91m"), 
+        BLACK("\033[30m"),
+        BLUE("\033[34m"),
+        GREEN("\033[92m"),
+        YELLOW("\033[93m"),
+        WHITE("\033[97m"),
+        ORANGE("\033[38;5;208m"),
+        DEFAULT("\033[39m"),
+        
+        //background colors
+        BLACKB("\033[40m"),
+        REDB("\033[41m"),
+        GREENB("\033[42m"),
+        YELLOWB("\033[43m"),
+        BLUEB("\033[44m"),
+        WHITEB("\033[107m"),
+        DEFAULTB("\033[49m")
+        
+        private String value
+        
+        Color(String value) {
+            this.value = value
+        }
+        String getValue() {
+            value
+        }
+    }
+
+    static void printColor(Object self, String content, String textColor, String backgroundColor){
+        Color text = textColor
+        Color background = backgroundColor
+        ansiColor('xterm') {
+            self.println(background.value + text.value + content + "\033[0m")
+        }
+    }
+
+    static void printError(Object self, String content){
+        self.printColor(this, "[ERROR] " + content, "RED", "DEFAULTB")
+    }
+
+    static void printDebug(Object self, String content){
+        self.printColor(this, "[DEBUG CRITICAL] " + content, "RED", "DEFAULTB")
+    }
+
+    static void printInfo(Object self, String content){
+        self.printColor(this, "[INFO] " + content, "BLUE", "DEFAULTB")
+    }
+
+    static void printWarning(Object self, String content){
+        self.printColor(this, "[WARNING] " + content, "ORANGE", "DEFAULTB")
+    }
+
     static int getTimeoutFromXML(Object self, String tests, String keyword, Integer additional_xml_timeout) {
         try {
             Integer xml_timeout = 0
