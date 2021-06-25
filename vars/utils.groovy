92514b9f9c441a4d72d9895ea3aee6027725fa99
Flame0n <36414392+Flame0n@users.noreply.github.com>
2021-06-25 15:42:20 +0300

    Prints has been replaced

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -2,64 +2,64 @@ import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 import hudson.model.Result
 import groovy.json.JsonOutput
 
+Color{
+    //text colors
+    RED("\033[91m"), 
+    BLACK("\033[30m"),
+    BLUE("\033[34m"),
+    GREEN("\033[92m"),
+    YELLOW("\033[93m"),
+    WHITE("\033[97m"),
+    ORANGE("\033[38;5;208m"),
+    DEFAULT("\033[39m"),
+    
+    //background colors
+    BLACKB("\033[40m"),
+    REDB("\033[41m"),
+    GREENB("\033[42m"),
+    YELLOWB("\033[43m"),
+    BLUEB("\033[44m"),
+    WHITEB("\033[107m"),
+    DEFAULTB("\033[49m")
+    
+    private String value
+    
+    Color(String value) {
+        this.value = value
+    }
+    String getValue() {
+        value
+    }
+}
+
+void printColor(Object self, String content, String textColor, String backgroundColor){
+    Color text = textColor
+    Color background = backgroundColor
+    self.ansiColor('xterm') {
+        self.println(background.value + text.value + content + "\033[0m")
+    }
+}
+
+void printError(Object self, String content){
+    printColor(self, "[ERROR] " + content, "RED", "DEFAULTB")
+}
+
+void printDebug(Object self, String content){
+    printColor(self, "[DEBUG CRITICAL] " + content, "RED", "DEFAULTB")
+}
+
+void printInfo(Object self, String content){
+    printColor(self, "[INFO] " + content, "BLUE", "DEFAULTB")
+}
+
+void printWarning(Object self, String content){
+    printColor(self, "[WARNING] " + content, "ORANGE", "DEFAULTB")
+}
+
 /**
  * self in methods params is a context of executable pipeline. Without it you can't call Jenkins methods.
  */
 class utils {
-
-    enum Color{
-        //text colors
-        RED("\033[91m"), 
-        BLACK("\033[30m"),
-        BLUE("\033[34m"),
-        GREEN("\033[92m"),
-        YELLOW("\033[93m"),
-        WHITE("\033[97m"),
-        ORANGE("\033[38;5;208m"),
-        DEFAULT("\033[39m"),
-        
-        //background colors
-        BLACKB("\033[40m"),
-        REDB("\033[41m"),
-        GREENB("\033[42m"),
-        YELLOWB("\033[43m"),
-        BLUEB("\033[44m"),
-        WHITEB("\033[107m"),
-        DEFAULTB("\033[49m")
-        
-        private String value
-        
-        Color(String value) {
-            this.value = value
-        }
-        String getValue() {
-            value
-        }
-    }
-
-    static void printColor(Object self, String content, String textColor, String backgroundColor){
-        Color text = textColor
-        Color background = backgroundColor
-        self.ansiColor('xterm') {
-            self.println(background.value + text.value + content + "\033[0m")
-        }
-    }
-
-    static void printError(Object self, String content){
-        utils.printColor(self, "[ERROR] " + content, "RED", "DEFAULTB")
-    }
-
-    static void printDebug(Object self, String content){
-        utils.printColor(self, "[DEBUG CRITICAL] " + content, "RED", "DEFAULTB")
-    }
-
-    static void printInfo(Object self, String content){
-        utils.printColor(self, "[INFO] " + content, "BLUE", "DEFAULTB")
-    }
-
-    static void printWarning(Object self, String content){
-        utils.printColor(self, "[WARNING] " + content, "ORANGE", "DEFAULTB")
-    }
 
     static int getTimeoutFromXML(Object self, String tests, String keyword, Integer additional_xml_timeout) {
         try {
