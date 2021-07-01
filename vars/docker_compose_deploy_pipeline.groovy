140905bfd59eedaaba1110b226594b60de325ca7
Artemy Kulbako <kulbakowork@mail.ru>
2021-07-01 15:15:43 +0300

    cleanup after docker deploy (#642)

--- vars/docker_compose_deploy_pipeline.groovy
+++ vars/docker_compose_deploy_pipeline.groovy
@@ -9,7 +9,7 @@
 * @param type deploy type (same as dir name in your project deploy dir {repoRoot}/deploy/{type})
 * @param gitUrlCredentialsId Jenkins credentialsId with string Url of git host (http://git.example.com)
 * @param gitUserCredentialsId Jenkins credentialsId with user access data for git host
-* @param composePath custom docker-compose file path from project root (if set, type is ingored) (some-dir/another/docker-compose.yml)
+* @param composePath custom docker-compose file path from project root (if set, type is ignored) (some-dir/another/docker-compose.yml)
 */
 def call(
     String project,
@@ -19,42 +19,31 @@ def call(
     String type = "custom",
     String gitUrlCredentialsId = "gitlabURL",
     String gitUserCredentialsId = "radeonprorender-gitlab",
-    String composePath = null
+    String composePath = "deploy/${type}/docker-compose.yml"
 ) {
-    def gitServerUrl
-    withCredentials([string(credentialsId: gitUrlCredentialsId, variable: "GIT_URL")]) {
-        gitServerUrl = GIT_URL
-    }
-
+    def gitServerUrl = withCredentials([string(credentialsId: gitUrlCredentialsId, variable: "GIT_URL")]) { GIT_URL }
     def jnPath = "${type}/${project}"
-    if (composePath == null) {
-        composePath = "deploy/${type}/docker-compose.yml"
-    }
     def urlParts = [ gitServerUrl, repoGroup, "${project}.git" ]
-    def repositoryUrl = urlParts.findAll({ it != null }).join("/")
-
-    println("[INFO] Project repository: ${repositoryUrl}")
-    println("[INFO] Project branch: ${branch}")
-    println("[INFO] Docker-compose file path: ${composePath}")
-
+    def repositoryUrl = urlParts.findAll { it != null }.join("/")
+    println """
+        [INFO] Project repository: ${repositoryUrl}
+        [INFO] Project branch: ${branch}
+        [INFO] Docker-compose file path: ${composePath}
+    """
     node(nodeName) {
         dir(jnPath) {
-            try {
-                sh "sudo docker-compose -f ${composePath} stop";
-                sh "sudo docker-compose -f ${composePath} rm --force"
-            } catch(Exception e) {
-                println("[WARNING] Catch an exception while docker stopping")
-                println(e.toString())
-                println(e.getMessage())
-            }
-
             checkoutScm(branchName: branch, repositoryUrl: repositoryUrl, credentialsId: gitUserCredentialsId)
-
             stage('Build') {
                 sh "sudo docker-compose -f ${composePath} build"
             }
             stage('Up') {
                 sh "sudo docker-compose -f ${composePath} up -d"
+            }
+            stage("Cleanup") {
+                sh """
+                    docker volume prune -f
+                    docker image prune -f
+                """
             }
         }
     }
