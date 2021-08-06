759111cea926b941e27db291d36615ba04604a7a
Wieceslaw <slava.astra@yahoo.com>
2021-08-06 12:14:13 +0500

    Add exception to check retries

--- vars/rpr_usdviewer_pipeline.groovy
+++ vars/rpr_usdviewer_pipeline.groovy
@@ -102,6 +102,7 @@ def installInventorPlugin(String osName, Map options, Boolean cleanInstall=true,
         if (customPathInstall) {
             logPostfix = "custom_path"
             dirOption = "/DIR=\"${CUSTOM_INSTALL_PATH}\""
+            throw new Exception("[DEBUG] Exception while custom path install phase to check instalaltions retries")
         } else {
             logPostfix = "clean"
         }
