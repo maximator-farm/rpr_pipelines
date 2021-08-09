13e7097a0a2076fd5f9aeb056ee374ee7419a290
Ilia Nemankov <INemankov@luxoft.com>
2021-08-09 16:39:47 +0300

    Draft implementation of Hybrid Paragon building

--- vars/rpr_hybrid_paragon_pipeline.groovy
+++ vars/rpr_hybrid_paragon_pipeline.groovy
@@ -12,7 +12,23 @@ def executeBuildWindows(Map options) {
 
     dir("RPRHybrid-UE") {
         checkoutScm(branchName: options.ueBranch, repositoryUrl: options.ueRepo)
-    }   
+    }
+
+    // download build scripts
+    downloadFiles("/volume1/CIS/bin-storage/ParagonBuildScripts/*", ".")
+
+    bat("if not exists PARAGON_BINARY mkdir PARAGON_BINARY")
+
+    bat("1_UpdateRPRHybrid.bat >> \"1_UpdateRPRHybrid.log\" 2>&1")
+    bat("2_CopyDLLsFromRPRtoUE.bat >> \"2_CopyDLLsFromRPRtoUE.log\" 2>&1")
+    bat("3_UpdateUE4.bat >> \"3_UpdateUE4.log\" 2>&1")
+    bat("4_PackageParagon.bat >> \"4_PackageParagon.log\" 2>&1")
+
+    dir("PARAGON_BINARY\\WindowsNoEditor") {
+        String ARTIFACT_NAME = "ParagonGame.zip"
+        bat(script: '%CIS_TOOLS%\\7-Zip\\7z.exe a' + " \"${ARTIFACT_NAME}\" .")
+        makeArchiveArtifacts(name: ARTIFACT_NAME, storeOnNAS: options.storeOnNAS)
+    }
 }
 
 
@@ -31,8 +47,6 @@ def executeBuild(String osName, Map options) {
                     println("${osName} is not supported")
             }
         }
-
-        // TODO do stash
     } catch (e) {
         println(e.getMessage())
         throw e
@@ -42,7 +56,9 @@ def executeBuild(String osName, Map options) {
 }
 
 def executePreBuild(Map options) {
-    checkoutScm(branchName: options.projectBranch, repositoryUrl: options.projectRepo, disableSubmodules: true)
+    dir("RPRHybrid") {
+        checkoutScm(branchName: options.projectBranch, repositoryUrl: options.projectRepo, disableSubmodules: true)
+    }
 
     options.commitAuthor = bat (script: "git show -s --format=%%an HEAD ",returnStdout: true).split('\r\n')[2].trim()
     commitMessage = bat (script: "git log --format=%%B -n 1", returnStdout: true)
@@ -61,7 +77,7 @@ def executePreBuild(Map options) {
 
 
 def call(String projectBranch = "",
-         String ueBranch = "",
+         String ueBranch = "rpr_material_serialization_particles",
          String platforms = "Windows") {
 
     multiplatform_pipeline(platforms, this.&executePreBuild, this.&executeBuild, null, null,
@@ -74,6 +90,7 @@ def call(String projectBranch = "",
                             TESTER_TAG:"HybridTester",
                             executeBuild:true,
                             executeTests:true,
+                            BUILD_TIMEOUT: 180,
                             retriesForTestStage:1,
                             storeOnNAS: true])
 }
