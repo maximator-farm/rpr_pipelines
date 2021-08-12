3f62b8cdbec014227401f03b9b0cd5d09fceb64b
Wieceslaw <slava.astra@yahoo.com>
2021-08-12 16:04:03 +0500

    Remove whitespace escaping due to download files changes

--- vars/makeUnstash.groovy
+++ vars/makeUnstash.groovy
@@ -35,9 +35,9 @@ def call(Map params) {
                     print("Try to make unstash №${retries}")
                     withCredentials([string(credentialsId: "nasURL", variable: "REMOTE_HOST")]) {
                         if (isUnix()) {
-                            status = sh(returnStatus: true, script: '$CIS_TOOLS/downloadFiles.sh' + " \"${remotePath.replace(" ", "\\\\\\ ")}\" . " + '$REMOTE_HOST')
+                            status = sh(returnStatus: true, script: '$CIS_TOOLS/downloadFiles.sh' + " \"${remotePath}\" . " + '$REMOTE_HOST')
                         } else {
-                            status = bat(returnStatus: true, script: '%CIS_TOOLS%\\downloadFiles.bat' + " \"${remotePath.replace(" ", "\\\\\\ ")}\" . " + '%REMOTE_HOST%')
+                            status = bat(returnStatus: true, script: '%CIS_TOOLS%\\downloadFiles.bat' + " \"${remotePath}\" . " + '%REMOTE_HOST%')
                         }
                     }
 
