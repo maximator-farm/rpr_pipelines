72ebeb65173173734023f29eb91bc237f5c84226
imaduabu <Iffy.Maduabuchi@amd.com>
2021-10-06 14:14:06 -0400

    Updated to have autojob proper applications

--- vars/amfdev_streaming_sdk_pipeline.groovy
+++ vars/amfdev_streaming_sdk_pipeline.groovy
@@ -17,21 +17,21 @@ import TestsExecutionType
 @Field final String AMF_THIRDPARTY = "drivers\\amf\\Thirdparty"
 //'games' : 'LoL,HeavenDX11,ApexLegends,ValleyDX11'
 @Field final def LUXSDK_AUTOJOB_CONFIG = [
-      'projectBranch' :             '',
-      'testsBranch' :               'master',
+      'projectBranch' :             'origin/amd/stg/amf',
+      'testsBranch' :               'origin/master',
       'platforms' :                 'Windows:Navi23',
       'clientTag' :                 'LuxSDK_Client',
       'winBuildConfiguration' :     'release',
       'winVisualStudioVersion' :    '2019',
       'winTestingBuildName' :       'release_vs2019',
+      'androidBuildConfiguration' : 'release,debug',
+      'games' :                     'LoL,HeavenDX9,HeavenDX11,ValleyDX11',
       'testsPackage' :              'General.json',
-      'tests' :                     '',
+      'tests' :                     'General',
       'testerTag' :                 'StreamingSDK',
       'testCaseRetries' :           2,
       'clientCollectTraces' :       false,
       'serverCollectTraces' :       false,
-      'games' :                     'HeavenDX11',
-      'androidBuildConfiguration' : 'release,debug',
       'storeOnNAS' :                false
   ]
 
