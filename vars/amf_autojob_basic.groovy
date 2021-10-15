6128676fb36f8ae84f9d13bd43e9b1c80da46885
imaduabu <Iffy.Maduabuchi@amd.com>
2021-10-15 15:58:20 -0400

    Trying another configuration

--- vars/amf_autojob_basic.groovy
+++ vars/amf_autojob_basic.groovy
@@ -1,3 +1,25 @@
-import static vars.autojobconfig.getConfig as getConfig
+import jenkins.model.Jenkins
 
-print(getConfig())
+def call() {
+	LUXSDK_AUTOJOB_CONFIG = [
+      'projectBranch' :             'origin/amd/stg/amf',
+      'testsBranch' :               'origin/master',
+      'platforms' :                 'Windows:Navi23',
+      'clientTag' :                 'LuxSDK_Client',
+      'winBuildConfiguration' :     'release',
+      'winVisualStudioVersion' :    '2019',
+      'winTestingBuildName' :       'release_vs2019',
+      'androidBuildConfiguration' : 'release,debug',
+      'games' :                     'LoL,HeavenDX9,HeavenDX11,ValleyDX11,ValleyDX9',
+      'testsPackage' :              'General.json',
+      'tests' :                     'General',
+      'testerTag' :                 'StreamingSDK',
+      'testCaseRetries' :           2,
+      'clientCollectTraces' :       false,
+      'serverCollectTraces' :       false,
+      'storeOnNAS' :                false
+    ]
+
+    print(str(LUXSDK_AUTOJOB_CONFIG))
+        
+}
